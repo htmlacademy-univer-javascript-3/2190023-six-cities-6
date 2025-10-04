a1a91d684926ec3f9b84c99210607de28fd90c7f
kradunches <sasha10131310@gmail.com>
2025-10-04 14:22:38 +0300

    react router task completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router';
 import { MainPage } from './pages/MainPage';
+import { LoginPage } from './pages/LoginPage';
+import { FavoritesPage } from './pages/FavoritesPage';
+import { PropertyPage } from './pages/PropertyPage';
+import { NotFound } from './components/NotFound';
+import { PrivateRoute } from './components/PrivateRoute';
 
 type AppProps = {
     offersCount: number;
 }
 
-const App: React.FC<AppProps> = ({ offersCount }) => {
-    return <MainPage offersCount={offersCount} />;
-}
+const App: React.FC<AppProps> = ({ offersCount }) => (
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<MainPage offersCount={offersCount} />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route 
+                path="/favorites" 
+                element={
+                    <PrivateRoute isAuthorized={false}>
+                        <FavoritesPage />
+                    </PrivateRoute>
+                } 
+            />
+            <Route path="/offer/:id" element={<PropertyPage />} />
+            <Route path="*" element={<NotFound />} />
+        </Routes>
+    </BrowserRouter>
+);
 
 export default App;
---
+++ src/components/NotFound.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const NotFound: React.FC = () => {
+    return (
+        <div>
+            <div>404 not found</div>
+            <a href="/">Go to main page</a>
+        </div>
+    );
+}
