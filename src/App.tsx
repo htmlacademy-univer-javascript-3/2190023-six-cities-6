d02697c59586c88f348dcbdebd92e7fbc3777ee7
kradunches <sasha10131310@gmail.com>
2025-10-05 16:57:44 +0300

    hooks task completed

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,22 @@ import { FavoritesPage } from './pages/FavoritesPage';
 import { PropertyPage } from './pages/PropertyPage';
 import { NotFound } from './components/NotFound';
 import { PrivateRoute } from './components/PrivateRoute';
+import type { Offer } from './mocks/offers';
 
 type AppProps = {
-    offersCount: number;
+    offers: Offer[];
 }
 
-const App: React.FC<AppProps> = ({ offersCount }) => (
+const App: React.FC<AppProps> = ({ offers }) => (
     <BrowserRouter>
         <Routes>
-            <Route path="/" element={<MainPage offersCount={offersCount} />} />
+            <Route path="/" element={<MainPage offers={offers} />} />
             <Route path="/login" element={<LoginPage />} />
             <Route 
                 path="/favorites" 
                 element={
-                    <PrivateRoute isAuthorized={false}>
-                        <FavoritesPage />
+                    <PrivateRoute isAuthorized={true}>
+                        <FavoritesPage offers={offers} />
                     </PrivateRoute>
                 } 
             />
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
+import { offers } from './mocks/offers';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-
-root.render(
-  <React.StrictMode>
-    <h1>Hello, World!</h1>
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <React.StrictMode>
+        <App offers={offers}/>
+    </React.StrictMode>
+)
