f6d4b45ab42e6ce0bdeffe7eb0d9b56b7b81023c
kradunches <sasha10131310@gmail.com>
2025-11-16 21:01:25 +0300

    gh pages fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/2190023-six-cities-6/',
   test: {
     environment: 'jsdom',
     browser: {
